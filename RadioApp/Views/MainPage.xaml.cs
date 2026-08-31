a39029b09ede89775cf9acfe970e3e5359cbf458
Michał Jaśkiewicz <michaljaskiewicz.developer@protonmail.com>
2026-08-31 21:55:06 +0200

    up

---
+++ RadioApp/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace RadioApp.Views;
+
+public partial class MainPage
+{
+    private int Count { get; set; }
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object? sender, EventArgs e)
+    {
+    }
+}
