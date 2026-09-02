326b52db56d6735b54139c5a4d6617353dd790a9
Michał Jaśkiewicz <108977086+mjaskiewicz1@users.noreply.github.com>
2026-09-02 18:53:26 +0200

    feat(ui): Add radio playback with LibVLC (#3)

--- RadioApp/MauiProgram.cs
+++ RadioApp/MauiProgram.cs
@@ -1,10 +1,13 @@
 ﻿using Core.Interfaces;
 using Core.Services;
 
+using LibVLCSharp.MAUI;
+
 using Microsoft.Extensions.Logging;
 
 using RadioApp.ViewModels;
 using RadioApp.Views;
+
 
 namespace RadioApp;
 
@@ -13,20 +16,17 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        }).UseLibVLCSharp();
         builder.Services.AddSingleton<IRadioDirectoryService, RadioDirectoryService>();
         builder.Services.AddSingleton<MainViewModel>();
         builder.Services.AddSingleton<MainPage>();
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
-
         return builder.Build();
     }
 }
--- RadioApp/Views/Components/RecommendedStationsView.xaml.cs
+++ RadioApp/Views/Components/RecommendedStationsView.xaml.cs
@@ -1,3 +1,7 @@
+using Core.Models;
+
+using RadioApp.ViewModels;
+
 namespace RadioApp.Views.Components;
 
 public partial class RecommendedStationsView
@@ -5,5 +9,14 @@ public partial class RecommendedStationsView
     public RecommendedStationsView()
     {
         InitializeComponent();
+    }
+
+    private void OnStationTapped(object? sender, TappedEventArgs e)
+    {
+        if (sender is not Grid { BindingContext: RadioStation station })
+            return;
+
+        if (BindingContext is MainViewModel viewModel)
+            viewModel.SelectedStation = station;
     }
 }
